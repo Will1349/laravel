1b0e23d5431db13e83253a0760178765fb41f62b
Neo Abad <willabad97@gmail.com>
2022-07-01 18:06:36 -0500

    Antes de empezar leecion 10 Aprendible

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+
+Route::get('/', function () {
+    return view('home');
+})->name('home');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/portfolio', function () {
+    return view('portfolio');
+})->name('portfolio');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
