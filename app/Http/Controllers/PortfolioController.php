6eff4485317b458a70ce309acfb5854da934a3ea
Neo Abad <willabad97@gmail.com>
2022-07-03 23:42:02 -0500

    Creaci'on del controlador "PortfolioController" con el método "php artisan make:controller PortfolioController -r"

---
+++ app/Http/Controllers/PortfolioController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PortfolioController extends Controller
+{
+    /**
+     * Display a listing of the Projects of PORTAFOLIO.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $portfolio = [
+            ['title' => 'Project #1'],
+            ['title' => 'Project #2'],
+            ['title' => 'Project #3'],
+            ['title' => 'Project #4'],
+        ];
+        return view('portfolio', compact('portfolio'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,8 @@
 <?php
 
-$portfolio = [
-    ['title' => 'Proyecto #1'],
-    ['title' => 'Proyecto #2'],
-    ['title' => 'Proyecto #3'],
-    ['title' => 'Proyecto #4'],
-];
+use App\Http\Controllers\PortfolioController;
 
 Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
-Route::view('/portfolio', 'portfolio', compact('portfolio'))->name('portfolio');
+Route::get('/portfolio', [PortfolioController::class, 'index']);
 Route::view('/contact', 'contact')->name('contact');
