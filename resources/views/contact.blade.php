547cbfaecd0d968848b28a0e35426d259615849b
Neo Abad <willabad97@gmail.com>
2022-07-04 09:18:27 -0500

    Validaci'on de Formularios

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,10 +6,18 @@
 <h1>Contact</h1>
 <form method="POST" action=" {{  route('contact')}}">
     @csrf
-    <input name=" name" placeholder="Input your name" value="Neo""><br />
-    <input type=" email" name="email" placeholder="Input your email address" value="neo@icloud.com"><br />
-    <input name="subject" placeholder="Input your subject" value="Asunto de prueba"><br />
-    <textarea name="content" placeholder="Input your message">Mensaje de prueba</textarea><br />
+
+    <input name=" name" placeholder="Input your name" value="{{ old('name') }}"><br />
+    {!! $errors->first('name', '<small>:message</small><br>') !!}
+
+    <input type=" email" name="email" placeholder="Input your email address" value="{{ old('email') }}"><br />
+    {!! $errors->first('email', '<small>:message</small><br>') !!}
+
+    <input name="subject" placeholder="Input your subject" value="{{ old('subject') }}"><br />
+    {!! $errors->first('subject', '<small>:message</small><br>') !!}
+
+    <textarea name="content" placeholder="Input your message">{{ old('content') }}</textarea><br />
+    {!! $errors->first('content', '<small>:message</small><br>') !!}
     <button>Send </button>
 </form>
 @endsection
