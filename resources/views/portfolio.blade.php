cfaad78b74c3181e3b47f70c9b47ecaa837f269f
Neo Abad <willabad97@gmail.com>
2022-07-06 16:07:37 -0500

    Migraciones con "php artisan migrate' y Eloquent para obtener datos

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -6,13 +6,12 @@
 <h1>Portfolio</h1>
 
 <ul>
-    @forelse($portfolio as $portfolioItem)
-    <li>{{ $portfolioItem['title'] }}
-    </li>
+    @forelse($projects as $project)
+    <li>{{ $project->title }} </li>
     @empty
     <li>No hay proyectos para mostrar</li>
     @endforelse
-
+    {{ $projects->links() }}
 </ul>
 
 @endsection
