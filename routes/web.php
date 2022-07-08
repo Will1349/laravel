8379423d2e4b1ff3c817f4a8feb968cd4501ddaa
Neo Abad <willabad97@gmail.com>
2022-07-07 23:18:33 -0500

    Mejorando el archivo de rutas

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,18 @@ use App\Http\Controllers\MessageController;
 Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
 
+Route::resource('portafolio', ProjectController::class)
+    ->parameters(['portafolio' => 'project'])
+    ->names('projects');
 
-Route::get('/portfolio', [ProjectController::class, 'index'])->name('projects.index');
-Route::get('/portfolio/create', [ProjectController::class, 'create'])->name('projects.create');
-Route::get('/portfolio/{project}/editar', [ProjectController::class, 'edit'])->name('projects.edit');
-Route::patch('/portfolio/{project}', [ProjectController::class, 'update'])->name('projects.update');
-Route::post('/portfolio', [ProjectController::class, 'store'])->name('projects.store');
-Route::get('/portfolio/{project}', [ProjectController::class, 'show'])->name('projects.show');
-Route::delete('/portfolio/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
+
+//Route::get('/portfolio', [ProjectController::class, 'index'])->name('projects.index');
+//Route::get('/portfolio/create', [ProjectController::class, 'create'])->name('projects.create');
+//Route::get('/portfolio/{project}/editar', [ProjectController::class, 'edit'])->name('projects.edit');
+//Route::patch('/portfolio/{project}', [ProjectController::class, 'update'])->name('projects.update');
+//Route::post('/portfolio', [ProjectController::class, 'store'])->name('projects.store');
+//Route::get('/portfolio/{project}', [ProjectController::class, 'show'])->name('projects.show');
+//Route::delete('/portfolio/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
 
 
 Route::view('/contact', 'contact')->name('contact');
