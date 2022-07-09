eb73ba9464342f74c7c50e49fdf37fc3f8e162c0
Neo Abad <willabad97@gmail.com>
2022-07-08 22:55:22 -0500

    Uso de Laravel Mix

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,13 +2,9 @@
 <html>
 
 <head>
-    <title>@yield('title')</title>
-    <style>
-        .active a {
-            color: red;
-            text-decoration: none;
-        }
-    </style>
+    <title>@yield('title','Aprendible')</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="{{ mix('js/app.js') }}" defer></script>
 </head>
 
 <body>
