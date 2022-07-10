5bed953a46bff7fcd8356f54426d81118e904e94
Neo Abad <willabad97@gmail.com>
2022-07-10 09:24:07 -0500

    Se agrega estilos para mostar una imagen proporcial alto ancho

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -24,7 +24,8 @@
         <div class="card border-0 shadow-sm mt-4 mx-auto" style="width: 18rem">
 
             @if ($project->image)
-            <img class="card-img-top" src="/storage/{{ $project->image }}" alt="{{ $project->title }}">
+            <img class="card-img-top" style="height: 150px; object-fit:cover" src="/storage/{{ $project->image }}"
+                alt="{{ $project->title }}">
             @endif
 
 
