922178094b8f2eb24419557fec9575b56ef5bb3c
Neo Abad <willabad97@gmail.com>
2022-07-10 08:36:17 -0500

    Cambio del tituo por defecto

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>@yield('title','Aprendible')</title>
+    <title>@yield('title','Isengard')</title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
