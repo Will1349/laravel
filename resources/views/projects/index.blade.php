2ba603617ffa7c9b5a2fa5342067ea298bfa330f
Neo Abad <willabad97@gmail.com>
2022-07-10 23:12:33 -0500

    Se cambia el style="width: 21rem para mostrar tres proyectos por cada fila

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -21,7 +21,7 @@
     <div class="d-flex flex-wrap justify-content-between align-items-start">
         @forelse($projects as $project)
 
-        <div class="card border-0 shadow-sm mt-4 mx-auto" style="width: 18rem">
+        <div class="card border-0 shadow-sm mt-4 mx-auto" style="width: 21rem">
 
             @if ($project->image)
             <img class="card-img-top" style="height: 150px; object-fit:cover" src="/storage/{{ $project->image }}"
