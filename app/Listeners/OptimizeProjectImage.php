55adc7a09de369f82932d22c3c4402dbf12a74de
Neo Abad <willabad97@gmail.com>
2022-07-10 23:04:02 -0500

    Se crea el listener OptimizeProjectImage y se añade la función handle para optimizar las imágenes

---
+++ app/Listeners/OptimizeProjectImage.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Listeners;
+use App\Events\ProjectSaved;
+use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class OptimizeProjectImage
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \App\Events\ProjectSaved  $event
+     * @return void
+     */
+    public function handle(ProjectSaved $event)
+    {
+        $image = Image::make(Storage::get($event->project->image))
+            ->widen(500)
+            ->limitColors(220)
+            ->encode();
+
+        Storage::put($event->project->image, (string) $image);
+    }
+}
