da3874bd6a209652dfd631d297fdb2c3cab007dc
Neo <iosneo@icloud.com>
2022-07-16 23:18:48 -0500

    Se corrige error de alerta en el boton cerrar

--- resources/views/partials/session-status.blade.php
+++ resources/views/partials/session-status.blade.php
@@ -2,7 +2,6 @@
 <div class="alert alert-primary alert-dismissible fade show" role="alert">
     <strong>{{ session('status') }}</strong>
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
     </button>
 </div>
 @endif
