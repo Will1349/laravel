335563d1648b73478d12acb918ad639dd310a6c0
Neo <iosneo@icloud.com>
2022-07-21 14:50:02 -0500

    Cambia la forma que se muestra los mensajes emerge

--- resources/views/partials/session-status.blade.php
+++ resources/views/partials/session-status.blade.php
@@ -1,5 +1,6 @@
 @if (session('status'))
-<div class="alert alert-primary alert-dismissible fade show" role="alert">
+<div class="alert alert-success alert-dismissible fade show w-30 col-12 col-sm-10 col-lg-6 mx-auto align-items-center"
+    role="alert">
     <strong>{{ session('status') }}</strong>
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
     </button>
