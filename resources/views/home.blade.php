06ae54d554b980c2cffb5bb8f5aa567ec42c977c
Neo <iosneo@icloud.com>
2022-07-22 13:22:14 -0500

    Se cambia el mensaje de la vista home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,16 +12,9 @@
                 ad,
                 sint ea tempore facere minus animi ullam tempora illo distinctio, adipisci harum placeat debitis a,
                 qui
-                est voluptatum dolore!dolor sit amet, consectetur adipisicing elit. Molestiae rerum
-                ad,
-                sint ea tempore facere minus animi ullam tempora illo distinctio, adipisci harum placeat debitis a,
-                qui
-                est voluptatum dolore! Lorem ipsum dolor sit amet consectetur adipisicing elit !dolor sit amet,
-                consectetur adipisicing elit. Molestiae rerum
-                ad,
-                sint ea tempore facere minus animi ullam tempora illo distinctio, adipisci harum placeat debitis a,
-                qui
-                est voluptatum dolore! Lorem ipsum dolor sit amet consectetur adipisicing eli.</p>
+                est voluptatum dolore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita harum illum
+                corporis neque doloremque necessitatibus est, accusamus asperiores natus blanditiis molestiae non
+                explicabo iste sit. Laudantium nulla fugit molestias accusamus?.</p>
             <hr>
             <a class="btn btn-lg w-100 btn-primary" href="{{ route('contact') }}">Contáctame</a>
             <hr>
